17642ca68045db2bdc6d43b523949d0faa86a462
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-18 05:51:43 -0800

    script.js: - Added a command that adds an event listener to the form for the submit event. - Added a command that prevents the typical form submission action from occurring. - Added a command that logs the current value of the text element to the console. - Added a command that logs the current value of the color input element (hex code) to the console. - Added a command that saves the current color input value to the local storage.

--- script.js
+++ script.js
@@ -7,4 +7,11 @@ document.addEventListener('DOMContentLoaded', () => { // Event listener for when
     if (SavedColor) { // If a saved color value exists
         console.log(`Saved color value: ${SavedColor}`); // Logs the saved color value to the console
     }
+
+    form.addEventListener('submit', (event) => { // Adds an event listener to the form for the submit event
+        event.preventDefault(); // Prevents the default form submission action from occurring
+        console.log(`Text Inside Text Box: ${TextInput.value}`); // Logs the current value of the text input element to the console
+        console.log(`Color Selector Hex Code: ${ColorSelector.value}`); // Logs the current value of the color input element to the console
+        localStorage.setItem('ColorSelector', ColorSelector.value); // Saves the current color input value to the local storage
+    }); // End of event listener for form submission
 }); // End of DOMContentLoaded event listener
