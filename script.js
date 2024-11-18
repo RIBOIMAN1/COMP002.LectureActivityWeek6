2ddcf4c22cd16c978461fa23b59b739737fc8fad
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-18 05:34:13 -0800

    index.html: - Added basic descriptive text in the form of an h1 element that describes what is going on in this assignment to the header. - Removed nav element and everything inside of it. - Added a form element with the following inside of it: - Added a form element containing an ID. - Added a label for the text box. - Added a text input element that contains an ID and name. - Added a label for the color input. - Added a color input element containing an ID and name. - Added a submit button.
    script.js:
    - Added a command serving as an event listener for when the DOM is fully loaded.
    - Added a command that gets the form element via ID.
    - Added a command that gets the text input element via ID.
    - Added a command that gets the color input element via ID.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => { // Event listener for when the DOM is fully loaded
+    const form = document.getElementById('myForm'); // Gets the form element via ID
+    const TextInput = document.getElementById('TextInput'); // Gets the text input element via ID
+    const ColorSelector = document.getElementById('ColorSelector'); // Gets the color input element via ID
+}); // End of the DOMContentLoaded event listener
